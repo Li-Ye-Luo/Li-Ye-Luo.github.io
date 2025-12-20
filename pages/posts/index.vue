e6d4b395d4702ca8e0f6594c195d75be47b536b1
MiQiLinBeiTaiQ <miqilinbeitai@gmail.com>
2025-12-20 23:49:22 +0800

    强行修改左右侧边栏

--- pages/posts/index.vue
+++ pages/posts/index.vue
@@ -14,6 +14,14 @@ useHead({
   <div class="mt-37 mb-4">
     <YunPostList />
   </div>
-
+  
+  <!-- 注意：如果使用 layout: default，布局通常自带 Footer。 -->
+  <!-- 如果发现有两个页脚，请删除下面这行 <YunFooter /> -->
   <YunFooter />
 </template>
+
+<!-- !!! 必须添加下面这个代码块 !!! -->
+<route lang="yaml">
+meta:
+  layout: default
+</route>
